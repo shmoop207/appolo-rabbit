1d165157af4d0c3c1d02d6b9e4ab4727c2ca297f
roman <shmoop207@gmail.com>
2019-09-11 20:14:56 +0300

    first

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import {Rabbit} from "./src/rabbit"
 import {Message} from "./src/messages/message"
 import {IOptions} from "./src/common/IOptions";
+import {IQueueOptions} from "./src/queues/IQueueOptions";
+import {IExchangeOptions} from "./src/exchanges/IExchangeOptions";
 import {App, createApp} from 'appolo-engine';
 
-export {Rabbit, Message,IOptions}
+export {Rabbit, Message, IOptions, IQueueOptions, IExchangeOptions}
 
 export async function createRabbit(options: IOptions) {
     let app = createApp({});
